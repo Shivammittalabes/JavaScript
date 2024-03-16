4ff509a6506aee477be605c25b491ff94a1e9f9d
ShivamMittal <98509372+Shivammittalabes@users.noreply.github.com>
2024-03-16 14:44:01 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 706095
+let accountEmail = "shivam@gmail.com"
+let accountState;
+var accountPassword = "706095"
+accountCity = "Jaipur"
+
+
+// accountId = 2  // not allowed
+
+accountEmail = "sm@gmail.com"
+accountPassword = "99118822"
+accountCity = "Bengalore"
+
+console.log(accountId);
+
+/*
+we should not use var because the issue of SCOPE
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; 
+// treat all JS code as newer version
+
+
+// alert( 3+3 ) 
+//we are using nodejs, not web browser [{ DC39, MDN }]
+// Code readability should be high
+
+let name = "Shivam" // String
+let age = 18  // Number
+let isLoggedIn = true //Boolean
+
+
+// Number => 2^53
+// bigint => for large no
+// string = ""
+// Boolean => true/false
+// null => stand alone value {representation of empty value}
+// undefined => value is not defined
+// symbol => for finding the uniqueness of component
+
+// object
+
+console.log(typeof "shivam");
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
