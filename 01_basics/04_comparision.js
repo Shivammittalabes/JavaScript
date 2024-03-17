2034c8c24c7297d033e880cfda8596f4899c1fdd
ShivamMittal <98509372+Shivammittalabes@users.noreply.github.com>
2024-03-17 18:22:45 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);  // only for concept understanding 
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+
+console.log( 2 === "2");
+console.log( 2 === 2);
+console.log( 2 === 3);
+
+console.log( "2" > 1);  // must use clean  code
+console.log( "02" > 1);
