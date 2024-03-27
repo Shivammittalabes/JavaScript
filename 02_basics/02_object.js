06e75e6579ca4e22e5a18f6c016f1af3c7859f6f
ShivamMittal <98509372+Shivammittalabes@users.noreply.github.com>
2024-03-27 09:41:11 +0000

    main

---
+++ 02_basics/02_object.js
@@ -0,0 +1,14 @@
+// SINGLETON  => made with constructor, but when we declare as litrals it is not singleton
+// Object.create => constructor method 
+
+
+// Object literals
+
+const jsUser = {
+    name: "shivam",
+    age: 23,
+    location: "muzaffarnagar",
+    email: "sm@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "tuesday"]
+}
